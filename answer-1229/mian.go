d553f24435a0c4a7e111e84e68c5990f008fcd45
黄旭 <36882534+Orangehi@users.noreply.github.com>
2021-01-01 12:15:34 +0800

    Update mian.go

--- answer-1229/mian.go
+++ answer-1229/mian.go
@@ -233,3 +233,205 @@ https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1014.md
 /*
 	本题可以通过上面的两个函数DaiToEth与EthToDai来发送交易
  */
+
+
+
+
+/*
+Question 3 (Optional):
+Find the best route path for swapping USDT to CRO (UniSwap)
+1.Estimate the number of CRO’s output from any given number of USDT’s.
+2.Estimate the number of USDT’s output from any given number of CRO’s.
+Requirements:
+1.Avoid the loss of precision as much as possible.
+2.Find the best route path.
+3.Sign and broadcast tx.
+Tips:
+1.This project will be helpful: https://github.com/Uniswap/uniswap-interface
+2.This class `sac/hooks/Trades.ts` is the key point.
+
+  	USDT属于erc20 token，为稳定币与美元挂钩
+	CRO同样属于ERC20 token，由Crypto.com发行的erc20 token
+	两者之间转换没有直接关系，需要先转换为WETH,然后在转换为对于ERC20 token
+	1.	USDT -> CRO
+	首先 通过调用合约方法（swapExactTokensForETH）将USDT转换为WETH,
+	然后将转换过来的WETH通过调用合约方法（swapExactETHForTokens）转换为CRO
+
+	2. 	CRO -> USDT
+	首先 通过调用合约方法（swapExactTokensForETH）将CRO转换为WETH,
+	然后将转换过来的WETH通过调用合约方法（swapExactETHForTokens）转换为USDT
+ */
+
+//CRO -> USDT
+func CroToUsdt(amounts int64)uint{
+	/*
+		先将CRO转换为WETH
+		swapExactTokensForETH
+			function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+	  			external
+	 		 returns (uint[] memory amounts);
+			Swaps an exact amount of tokens for as much ETH as possible, along the route determined by the path. The first element of path is the input token, the last must be WETH, and any intermediate elements represent intermediate pairs to trade through (if, for example, a direct pair does not exist).
+
+			If the to address is a smart contract, it must have the ability to receive ETH.
+			Name	Type
+			amountIn	uint	The amount of input tokens to send.
+			amountOutMin	uint	The minimum amount of output tokens that must be received for the transaction not to revert.
+			path	address[] calldata	An array of token addresses. path.length must be >= 2. Pools for each consecutive pair of addresses must exist and have liquidity.
+			to	address	Recipient of the ETH.
+			deadline	uint	Unix timestamp after which the transaction will revert.
+			amounts	uint[] memory	The input token amount and all subsequent output token amounts.
+	 */
+	tx1 ,err := chain.SendRewardTransaction(from,to,amounts,nil)
+	if err != nil{
+		return 0
+	}
+
+	//golang没有原生的web3库函数,智能通过修改智能合约结构(增加emit事件将结果返回)，然后通过解析交易回执的logs中的Topics拿到amounts字段
+	r1,err := chain.GetRewardTransactionReceipt(tx1.String())
+	if err != nil{
+		return 0
+	}
+
+	//
+	if len(r1.Logs)==0 {
+		return 0
+	}
+	if len(r1.Logs[0].Topics) == 0{
+		return 0
+	}
+	//这里拿到了WETH的数量
+	s1, err := strconv.ParseInt(r.Logs[0].Topics[0].String(), 16, 32)
+	if err != nil{
+		return 0
+	}
+
+	/*
+		然后将WETH转换为USDT
+				swapExactETHForTokens
+			function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+			  external
+			  payable
+			  returns (uint[] memory amounts);
+			Swaps an exact amount of ETH for as many output tokens as possible, along the route determined by the path. The first element of path must be WETH, the last is the output token, and any intermediate elements represent intermediate pairs to trade through (if, for example, a direct pair does not exist).
+
+			Name	Type
+			msg.value (amountIn)	uint	The amount of ETH to send.
+			amountOutMin	uint	The minimum amount of output tokens that must be received for the transaction not to revert.
+			path	address[] calldata	An array of token addresses. path.length must be >= 2. Pools for each consecutive pair of addresses must exist and have liquidity.
+			to	address	Recipient of the output tokens.
+			deadline	uint	Unix timestamp after which the transaction will revert.
+			amounts	uint[] memory	The input token amount and all subsequent output token amounts.
+	 */
+	tx2 ,err := chain.SendRewardTransaction(from,to, s1,nil)
+	if err != nil{
+		return 0
+	}
+
+	//golang没有原生的web3库函数,智能通过修改智能合约结构(增加emit事件将结果返回)，然后通过解析交易回执的logs中的Topics拿到amounts字段
+	r2,err := chain.GetRewardTransactionReceipt(tx2.String())
+	if err != nil{
+		return 0
+	}
+
+	//
+	if len(r2.Logs)==0 {
+		return 0
+	}
+	if len(r2.Logs[0].Topics) == 0{
+		return 0
+	}
+	//这里拿到了USDT的数量
+	s2, err := strconv.ParseInt(r.Logs[0].Topics[0].String(), 16, 32)
+	if err != nil{
+		return 0
+	}
+
+	return uint(s2)
+}
+
+//USDT -> CRO
+func UsdtToCro(amounts int64)uint{
+	/*
+			先将USDT转换为WETH
+			swapExactTokensForETH
+				function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+		  			external
+		 		 returns (uint[] memory amounts);
+				Swaps an exact amount of tokens for as much ETH as possible, along the route determined by the path. The first element of path is the input token, the last must be WETH, and any intermediate elements represent intermediate pairs to trade through (if, for example, a direct pair does not exist).
+
+				If the to address is a smart contract, it must have the ability to receive ETH.
+				Name	Type
+				amountIn	uint	The amount of input tokens to send.
+				amountOutMin	uint	The minimum amount of output tokens that must be received for the transaction not to revert.
+				path	address[] calldata	An array of token addresses. path.length must be >= 2. Pools for each consecutive pair of addresses must exist and have liquidity.
+				to	address	Recipient of the ETH.
+				deadline	uint	Unix timestamp after which the transaction will revert.
+				amounts	uint[] memory	The input token amount and all subsequent output token amounts.
+	*/
+	tx1 ,err := chain.SendRewardTransaction(from,to,amounts,nil)
+	if err != nil{
+		return 0
+	}
+
+	//golang没有原生的web3库函数,智能通过修改智能合约结构(增加emit事件将结果返回)，然后通过解析交易回执的logs中的Topics拿到amounts字段
+	r1,err := chain.GetRewardTransactionReceipt(tx1.String())
+	if err != nil{
+		return 0
+	}
+
+	//
+	if len(r1.Logs)==0 {
+		return 0
+	}
+	if len(r1.Logs[0].Topics) == 0{
+		return 0
+	}
+	//这里拿到了WETH的数量
+	s1, err := strconv.ParseInt(r.Logs[0].Topics[0].String(), 16, 32)
+	if err != nil{
+		return 0
+	}
+
+	/*
+		然后将WETH转换为CRO
+				swapExactETHForTokens
+			function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+			  external
+			  payable
+			  returns (uint[] memory amounts);
+			Swaps an exact amount of ETH for as many output tokens as possible, along the route determined by the path. The first element of path must be WETH, the last is the output token, and any intermediate elements represent intermediate pairs to trade through (if, for example, a direct pair does not exist).
+
+			Name	Type
+			msg.value (amountIn)	uint	The amount of ETH to send.
+			amountOutMin	uint	The minimum amount of output tokens that must be received for the transaction not to revert.
+			path	address[] calldata	An array of token addresses. path.length must be >= 2. Pools for each consecutive pair of addresses must exist and have liquidity.
+			to	address	Recipient of the output tokens.
+			deadline	uint	Unix timestamp after which the transaction will revert.
+			amounts	uint[] memory	The input token amount and all subsequent output token amounts.
+	*/
+	tx2 ,err := chain.SendRewardTransaction(from,to, s1,nil)
+	if err != nil{
+		return 0
+	}
+
+	//golang没有原生的web3库函数,智能通过修改智能合约结构(增加emit事件将结果返回)，然后通过解析交易回执的logs中的Topics拿到amounts字段
+	r2,err := chain.GetRewardTransactionReceipt(tx2.String())
+	if err != nil{
+		return 0
+	}
+
+	//
+	if len(r2.Logs)==0 {
+		return 0
+	}
+	if len(r2.Logs[0].Topics) == 0{
+		return 0
+	}
+	//这里拿到了CRO的数量
+	s2, err := strconv.ParseInt(r.Logs[0].Topics[0].String(), 16, 32)
+	if err != nil{
+		return 0
+	}
+
+	return uint(s2)
+}
